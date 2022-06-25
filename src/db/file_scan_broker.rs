52d75ff49ca7e5fc64ee46bf94c2f6d64b257056
Richard Garbi <richard@mobilgov.com>
2022-06-25 16:50:24 -0500

    start work on a function to get a file that needs hashing

--- src/db/file_scan_broker.rs
+++ src/db/file_scan_broker.rs
@@ -26,3 +26,29 @@ pub async fn insert_scan(file_scan: FileScan, pool: &PgPool) -> Result<Uuid, Err
 
     Ok(file_scan.id)
 }
+
+#[tracing::instrument(name = "Select a file that needs hashing", skip(file_scan, pool))]
+pub async fn select_a_file_that_needs_hashing(file_scan: FileScan, pool: &PgPool) -> Result<Uuid, Error> {
+    sqlx::query!(
+        r#"SELECT id, file_name, file_location, file_hash, posted_on, last_updated, status, being_worked, work_started
+            FROM file_scan
+            WHERE status =
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"#,
+        file_scan.id,
+        file_scan.file_name,
+        file_scan.file_location,
+        file_scan.file_hash,
+        file_scan.posted_on,
+        file_scan.last_updated,
+        file_scan.status.as_str(),
+        file_scan.being_worked,
+        file_scan.work_started,
+    ).execute(pool)
+        .await
+        .map_err(|e: Error| {
+            tracing::error!("{:?}", e);
+            e
+        })?;
+
+    Ok(file_scan.id)
+}
