8db9360b26cd5473a64d43695a84487e965bb2ab
Richard Garbi <richard@mobilgov.com>
2022-06-30 05:49:30 -0500

    more tests

--- tests/api/background_hasher_test.rs
+++ tests/api/background_hasher_test.rs
@@ -29,3 +29,39 @@ async fn post_file_to_file_scan_works() {
     assert_ne!(&file_scan_after_hash.file_hash, "");
     assert_eq!(&file_scan_after_hash.status, &ScanStatus::DoneHashing);
 }
+
+#[tokio::test]
+async fn post_file_to_file_scan_2x_does_the_right_thing() {
+    let app = spawn_app().await;
+
+    let input: &[u8] = include_bytes!("../../tests/test_files/sample_file_1.txt");
+    let body = Vec::from(input);
+    let response = app.post_scan(body).await;
+
+    // Assert
+    assert!(&response.status().is_success());
+    let file_scan_stored = to_file_scan_from_str(response.text().await.unwrap().as_str());
+
+    assert_eq!(&file_scan_stored.file_hash, "");
+    assert_eq!(&file_scan_stored.status, &ScanStatus::Pending);
+
+    hash_files(&app.db_pool).await;
+
+    let after_hash = select_a_file_hash_by_id(file_scan_stored.clone().id, &app.db_pool).await;
+    assert_ok!(&after_hash);
+
+    let file_scan_after_hash = after_hash.unwrap();
+
+    assert_ne!(&file_scan_after_hash.file_hash, "");
+    assert_eq!(&file_scan_after_hash.status, &ScanStatus::DoneHashing);
+
+    hash_files(&app.db_pool).await;
+
+    let after_hash_second_hash = select_a_file_hash_by_id(file_scan_stored.clone().id, &app.db_pool).await;
+    assert_ok!(&after_hash_second_hash);
+
+    let file_scan_after_second_hash = after_hash_second_hash.unwrap();
+
+    assert_ne!(&file_scan_after_second_hash.file_hash, "");
+    assert_eq!(&file_scan_after_second_hash.status, &ScanStatus::DoneHashing);
+}
