2b6eac58a5fdd4660c327945bdb1a1b852484e44
Richard Garbi <richard@mobilgov.com>
2022-07-08 19:33:07 -0500

    more work on the scanning portion

--- src/background/background_scanner.rs
+++ src/background/background_scanner.rs
@@ -1,9 +1,11 @@
 use sqlx::PgPool;
 use crate::db::file_scan_broker::select_a_file_that_needs_worked;
-use crate::domain::file_scan_model::{FileScan, ScanStatus};
+use crate::domain::file_scan_model::{FileScan, ScanResult, ScanStatus};
+use tracing::Level;
 
 pub static MINUTES_TO_WAIT_BEFORE_ATTEMPTING_TO_WORK_AGAIN: i64 = 10;
-pub async fn scan_files(_pg_pool: &PgPool) {
+
+pub async fn scan_files(pg_pool: &PgPool) {
     //get a lock on a file that has been hashed but has not been scanned.
     let get_file_result = select_a_file_that_needs_worked(
         ScanStatus::DoneHashing,
@@ -16,12 +18,7 @@ pub async fn scan_files(_pg_pool: &PgPool) {
     match get_file_result {
         Ok(maybe_a_file_scan) => {
             if maybe_a_file_scan.is_some() {
-                let file_scan = maybe_a_file_scan.unwrap();
-                //hash the file.
-                let file_hash = hash_a_file(file_scan.file_location.clone()).await;
-                //update the record
-                let _save_result =
-                    set_a_file_scan_to_be_done_hashing(file_scan.id, file_hash, pg_pool).await;
+               scan_file(&maybe_a_file_scan.unwrap()).await
             }
         }
         Err(err) => {
@@ -33,4 +30,6 @@ pub async fn scan_files(_pg_pool: &PgPool) {
     //update the record
 }
 
-pub async fn scan_file(file_scan: &FileScan) ->
+pub async fn scan_file(file_scan: &FileScan) -> Result<ScanResult, Error> {
+    Ok(ScanResult::Clean)
+}
