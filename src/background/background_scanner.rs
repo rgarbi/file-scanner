c17d205c831e8c4347da9e68b380e014a3397d90
Richard Garbi <richard@mobilgov.com>
2022-07-09 14:03:12 -0500

    a little more progress

--- src/background/background_scanner.rs
+++ src/background/background_scanner.rs
@@ -1,6 +1,8 @@
+use std::process::Stdio;
 use sqlx::PgPool;
 use crate::db::file_scan_broker::select_a_file_that_needs_worked;
 use crate::domain::file_scan_model::{FileScan, ScanResult, ScanStatus};
+use tokio::process::Command;
 use tracing::Level;
 
 pub static MINUTES_TO_WAIT_BEFORE_ATTEMPTING_TO_WORK_AGAIN: i64 = 10;
@@ -31,5 +33,29 @@ pub async fn scan_files(pg_pool: &PgPool) {
 }
 
 pub async fn scan_file(file_scan: &FileScan) -> Result<ScanResult, Error> {
+    let command = Command::new("ls").stdout(Stdio::null());
+
+    let child_process_handle = command.spawn();
+
+    match child_process_handle {
+        Ok(mut child) =>{
+            let result = child.wait().await;
+
+            match result {
+                Ok(exit_status) => {
+                    if exit_status.exit_ok().is_ok() {
+                        return Ok(ScanResult::Clean);
+                    }
+
+                    return Err(exit_status.)
+                }
+            }
+
+        },
+        Err(error) => {
+            return Err(error);
+        }
+    }
+
     Ok(ScanResult::Clean)
 }
