98a9b727af8dfe868c101979250fc4436b154940
Richard Garbi <richard@mobilgov.com>
2022-07-29 07:26:40 -0500

    get another table going

---
+++ migrations/20220729112625_creat-queue-item-table.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+-- Add migration script here
+CREATE TABLE queue_items(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    queue_item_type TEXT NOT NULL,
+    queue_item_contents TEXT NOT NULL,
+    work_started bigint NOT NULL,
+    being_worked bool NOT NULL DEFAULT false,
+    error_count int NOT NULL,
+    error_message TEXT
+);
+
+CREATE INDEX queue_items_being_worked_idx ON queue_items (queue_item_type, being_worked, work_started);
--- src/background/queue_item.rs
+++ src/background/queue_item.rs
@@ -6,9 +6,11 @@ use uuid::Uuid;
 pub struct QueueItem {
     pub id: Uuid,
     pub queue_item_type: String,
-    pub queue_item: String,
+    pub queue_item_contents: String,
     pub work_started: Option<i64>,
     pub being_worked: bool,
     pub error_count: i64,
     pub error_message: Option<String>,
 }
+
+
