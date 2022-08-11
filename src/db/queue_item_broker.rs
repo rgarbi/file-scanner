06eeaadeb8844c87f960b6a582a70b8650336193
Richard Garbi <richard@mobilgov.com>
2022-08-11 10:37:34 -0500

    pull a queue item

--- src/db/queue_item_broker.rs
+++ src/db/queue_item_broker.rs
@@ -1,6 +1,7 @@
 use crate::background::queue_item::QueueItem;
 use sqlx::{Error, PgPool};
 use uuid::Uuid;
+use crate::util::{get_unix_epoch_time_as_seconds, get_unix_epoch_time_minus_minutes_as_seconds};
 
 #[tracing::instrument(name = "Saving new file scan", skip(queue_item, pool))]
 pub async fn store(queue_item: QueueItem, pool: &PgPool) -> Result<Uuid, Error> {
@@ -24,3 +25,51 @@ pub async fn store(queue_item: QueueItem, pool: &PgPool) -> Result<Uuid, Error> 
 
     Ok(queue_item.id)
 }
+
+#[tracing::instrument(name = "Select and lock a file by status", skip(pool))]
+pub async fn get_item_that_needs_worked(
+    wait_period_in_minutes: i64,
+    pool: &PgPool,
+) -> Result<Option<QueueItem>, Error> {
+    let work_start_time = get_unix_epoch_time_as_seconds() as i64;
+    let abandoned_time =
+        get_unix_epoch_time_minus_minutes_as_seconds(wait_period_in_minutes) as i64;
+    let result = sqlx::query!(
+        r#"UPDATE queue_items
+            SET
+                being_worked = true,
+                work_started = $1
+            WHERE id = (
+                SELECT id
+                FROM queue_items
+                WHERE (being_worked = false) OR (work_started <= $2)
+                LIMIT 1
+                FOR UPDATE SKIP LOCKED
+            )
+            RETURNING *"#,
+        Some(work_start_time),
+        abandoned_time,
+    )
+        .fetch_optional(pool)
+        .await;
+
+    return match result {
+        Ok(res) => match res {
+            Some(row) => Ok(Some(QueueItem {
+                id: row.id,
+                queue_item_type: row.queue_item_type,
+                being_worked: row.being_worked,
+                error_count: row.error_count,
+                work_started: Some(row.work_started),
+                queue_item_contents: row.queue_item_contents,
+                error_message: row.error_message,
+            })),
+            None => Ok(None),
+        },
+        Err(e) => {
+            tracing::error!("{:?}", e);
+            Err(e)
+        }
+    };
+}
+
