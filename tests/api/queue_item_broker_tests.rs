33819a2f9c88279806f7e8fd5960cf38f4b75890
Richard Garbi <richard@mobilgov.com>
2022-08-19 10:57:55 -0500

    add a test

--- tests/api/queue_item_broker_tests.rs
+++ tests/api/queue_item_broker_tests.rs
@@ -3,7 +3,7 @@ use claim::{assert_err, assert_none, assert_ok, assert_some};
 use db::queue_item_broker::store;
 use file_scanner::background::MINUTES_TO_WAIT_BEFORE_ATTEMPTING_TO_WORK_AGAIN;
 use file_scanner::db;
-use file_scanner::db::queue_item_broker::get_item_that_needs_worked;
+use file_scanner::db::queue_item_broker::{get_item_that_needs_worked, put_item_back};
 use file_scanner::util::get_unix_epoch_time_minus_minutes_as_seconds;
 
 #[tokio::test]
@@ -89,4 +89,28 @@ async fn get_queue_item_that_is_expired_works() {
     assert_ok!(&get_item_result);
 
     assert_some!(get_item_result.unwrap());
+}
+
+#[tokio::test]
+async fn put_queue_item_back_works() {
+    let app = spawn_app(false).await;
+
+    let queue_item = generate_queue_item();
+    assert_ok!(store(queue_item.clone(), &app.db_pool).await);
+
+    let get_item_result = get_item_that_needs_worked(&app.db_pool).await;
+    assert_ok!(&get_item_result);
+    assert_some!(get_item_result.unwrap());
+
+    assert_ok!(put_item_back(queue_item, &app.db_pool).await);
+
+
+    let get_item_result_again = get_item_that_needs_worked(&app.db_pool).await;
+    assert_ok!(&get_item_result_again);
+
+    let item = get_item_result_again.unwrap();
+    assert_some!(&item);
+
+    assert_eq!(1, item.unwrap().error_count)
+
 }
